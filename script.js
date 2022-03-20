f581563ce6898ef1dc9e32e228714b2b7625f1a6
princentaba4@gmail.com <princentaba4@gmail.com>
2022-03-20 18:00:33 +0300

    13th commit declared our variables on java script

---
+++ script.js
@@ -0,0 +1,60 @@
+var small = {
+    name: "small",
+    price: 500,
+    }
+  
+var medium = {
+    name: "medium",
+    price: 800,
+    }
+  
+var large = {
+    name: "large",
+    price: 1000,
+    }
+
+var XL = {
+    name: "XL",
+    price: 1300, 
+    }
+
+var crispy = {
+    name: "crispy",
+    price: 100,
+    }
+
+var stuffed = {
+    name: "stuffed",
+    price: 150,
+    }
+
+var glutenfree = {
+    name: "gluten free",
+    price: 200,
+    }
+
+var macon = {
+    name: "macon",
+    price: 250,
+    }
+  
+var mince = {
+    name: "mince",
+    price: 200,
+    }
+  
+var pineapple = {
+    name: "pineapple",
+    price: 150,
+    }
+  
+var greenpepper = {
+    name: "greenpepper",
+    price: 100,
+    }
+
+var olives = {
+    name: "olives",
+    price: 100,
+    }
+  
